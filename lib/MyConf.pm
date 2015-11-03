283ed36cd0de8b220a23c9833c1c7ffc3375a2fa
Pattawan Kaewduangdee <pattawan.kaewduangdee@sidnet.info>
2015-11-03 15:39:14 +0100

    Singleton Moo

---
+++ lib/MyConf.pm
@@ -0,0 +1,18 @@
+package MyConf;
+use Moo;
+with 'MooX::Singleton';
+ 
+use Config::Tiny;
+ 
+has file => (is => 'ro', required => 1);
+has conf => (is => 'rw');
+ 
+sub BUILD {
+    my ($self) = @_;
+ 
+    my $conf = Config::Tiny->read($self->file, 'utf8')
+        or die sprintf "Could not get configuration from '%s'", $self->file;
+    $self->conf($conf);
+}
+
+1;
---
+++ script/application.pl
@@ -0,0 +1,26 @@
+use strict;
+use warnings;
+use 5.010;
+ 
+use MyConf;
+ 
+my $c = MyConf->instance( file => 'conf.ini' );
+say $c->file;
+ 
+my $d = MyConf->instance( file => 'data.ini' );
+say $d->file;
+ 
+my $w = MyConf->instance();
+say $w->file;
+ 
+my $o = MyConf->new( file => 'other.ini' );
+say $o->file;
+ 
+my $z = MyConf->new( file => 'zorg.ini' );
+say $z->file;
+
+
+
+# my $a_value = $c->conf->{'section A'}{key};
+
+# say $a_value;
